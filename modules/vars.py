ed986e75c3de518bc406ee096439e2592632eed6
priyanshu620-pd <priyanshudwivedi620@gmail.com>
2026-08-19 02:03:06 +0530

    Update API credentials and user details

--- modules/vars.py
+++ modules/vars.py
@@ -3,18 +3,18 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "28088290"))
-API_HASH = environ.get("API_HASH", "6998f2c585fdce65ac72dfa23d02b6ec")
+API_ID = int(environ.get("API_ID", "30574823"))
+API_HASH = environ.get("API_HASH", "2815bb996f64421716844acaf2d51493")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
-OWNER = int(environ.get("OWNER", "5850397219"))
+OWNER = int(environ.get("OWNER", "8549673687"))
 CREDIT = environ.get("CREDIT", '<a href="https://t.me/Jetha_lal_bot">𝄟⃝🐬🅹🅰🅸 🆂🅷🆁🅸 🆁🅰🅼 ⚡️ 𝄟⃝🐬 💻</a>')
 cookies_file_path = os.getenv("cookies_file_path", "youtube_cookies.txt")
 
-TOTAL_USER = os.environ.get('TOTAL_USERS', '5850397219').split(',')
+TOTAL_USER = os.environ.get('TOTAL_USERS', '8549673687').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
 
-AUTH_USER = os.environ.get('AUTH_USERS', '5850397219').split(',')
+AUTH_USER = os.environ.get('AUTH_USERS', '8549673687').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
